6acab53d7b45451e5889ad7d738137fcb7a8a27d
Gordon Petty <43971581+ghpetty@users.noreply.github.com>
2024-05-22 13:27:58 -0400

    First full version of TTL2Table script

--- TTL2Table/OE_TTL2Table.m
+++ TTL2Table/OE_TTL2Table.m
@@ -9,4 +9,28 @@ TTL_path = uigetdir(cd,"Select folder containing Open Ephys TTL (.npy) files");
 dataKey = readtable(fullfile(filepath,filename));
 dataKey.VariableName = string(dataKey.VariableName);
 %% Load the time stamps and states
-states = readNPY(fullfile(TTL_path,"states.npy"))
+states = readNPY(fullfile(TTL_path,"states.npy"));
+timestamps = readNPY(fullfile(TTL_path,"timestamps.npy"));
+%% Separate out events using the data key:
+varNames = dataKey.VariableName;
+[startIndCell,endIndCell] = deal(cell(height(dataKey),1));
+% Copy data key table and append onsets and offsets to it
+for i = 1:length(varNames)
+    startInds = timestamps(states == dataKey{i,"Key"});
+    if size(startInds,1) > 1
+        startInds = startInds';
+    end
+    startIndCell{i} = startInds;
+    % Offset is always key*-1
+    endInds = timestamps(states == -dataKey{i,"Key"});
+    if size(endInds,1) > 1
+        endInds = endInds';
+    end
+    endIndCell{i} = endInds;
+
+end
+
+BehaviorDataTable= [dataKey,cell2table([startIndCell,endIndCell],'VariableNames',{'StartInds','EndInds'})];
+BehaviorDataTable.Properties.RowNames = BehaviorDataTable.VariableName;
+BehaviorDataTable.VariableName = [];
+
